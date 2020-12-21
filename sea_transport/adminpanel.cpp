50aa95e65953b04ccf1edcec86f2641f143ec4c0
Andrew nuark G <mrcluster@ya.ru>
2020-12-21 21:49:40 +0700

    Admin panel slots

--- sea_transport/adminpanel.cpp
+++ sea_transport/adminpanel.cpp
@@ -1,9 +1,27 @@
 #include "adminpanel.h"
 #include "ui_adminpanel.h"
 
-AdminPanel::AdminPanel(QWidget *parent) : QMainWindow(parent), ui(new Ui::AdminPanel)
-{
+AdminPanel::AdminPanel(QWidget *parent) : QMainWindow(parent), ui(new Ui::AdminPanel) {
     ui->setupUi(this);
+
+    connect(ui->pb_logout, &QPushButton::clicked, this, &AdminPanel::on_logout_requested);
+
+    connect(ui->pb_vessels_add, &QPushButton::clicked, this, &AdminPanel::on_vessel_add);
+    connect(ui->pb_vessels_remove, &QPushButton::clicked, this, &AdminPanel::on_vessel_remove);
+
+    connect(ui->pb_users_add, &QPushButton::clicked, this, &AdminPanel::on_user_add);
+    connect(ui->pb_users_remove, &QPushButton::clicked, this, &AdminPanel::on_user_remove);
+
+    connect(ui->pb_users_add, &QPushButton::clicked, this, &AdminPanel::on_storage_add);
+    connect(ui->pb_users_remove, &QPushButton::clicked, this, &AdminPanel::on_storage_remove);
+
+    connect(ui->pb_dp_add, &QPushButton::clicked, this, &AdminPanel::on_delivery_point_add);
+    connect(ui->pb_dp_remove, &QPushButton::clicked, this, &AdminPanel::on_delivery_point_remove);
+
+//    ui->tv_vessels->setModel();
+//    ui->tv_users->setModel();
+//    ui->tv_dp->setModel();
+//    ui->tv_storages->setModel();
 }
 
 AdminPanel::~AdminPanel() {
@@ -12,5 +30,6 @@ AdminPanel::~AdminPanel() {
 
 AdminPanel& AdminPanel::set_user(const user_entity &user) {
     this->user = user;
+    ui->lab_user->setText(tr("Hello user %1").arg(user.login()));
     return *this;
 }
--- sea_transport/adminpanel.h
+++ sea_transport/adminpanel.h
@@ -21,6 +21,21 @@ public:
 
     AdminPanel& set_user(const user_entity &user);
 
+private slots:
+    void on_logout_requested();
+
+    void on_vessel_add();
+    void on_vessel_remove();
+
+    void on_user_add();
+    void on_user_remove();
+
+    void on_storage_add();
+    void on_storage_remove();
+
+    void on_delivery_point_add();
+    void on_delivery_point_remove();
+
 private:
     Ui::AdminPanel *ui;
 };
