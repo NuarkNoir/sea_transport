4a3951938aeed3b6a5bc2e33f0e6bd546c0b42b3
Andrew nuark G <mrcluster@ya.ru>
2020-12-23 01:23:36 +0700

    Vessel interaction update

--- sea_transport/entities/vessel_entity.cpp
+++ sea_transport/entities/vessel_entity.cpp
@@ -3,7 +3,7 @@
 
 entity_id vessel_entity::__global_id = 0;
 
-vessel_entity::vessel_entity(entity_id harbor_id, unsigned int capacity) : _harbor_id(harbor_id), _capacity(capacity) {
+vessel_entity::vessel_entity(entity_id skipper_id, entity_id harbor_id, unsigned int capacity) : _skipper_id(skipper_id), _harbor_id(harbor_id), _capacity(capacity) {
     this->_id = ++vessel_entity::__global_id;
 }
 
@@ -21,6 +21,27 @@ unsigned int vessel_entity::capacity() const {
 
 const QVector<cargo_entity> vessel_entity::cargo() {
     return this->_cargo;
+}
+
+void vessel_entity::add_cargo(cargo_entity object, bool &success) {
+    success = ((int)this->_capacity - (int)object.volume()) > 0;
+    if (success) {
+        this->_cargo.push_back(object);
+        this->_capacity -= object.volume();
+    }
+}
+
+void vessel_entity::withdraw_cargo(entity_id oid, bool &success) {
+    success = false;
+    auto vit = this->_cargo.begin();
+    for (; vit != this->_cargo.end(); vit++) {
+        if ((*vit).id() == oid) {
+            this->_capacity += (*vit).volume();
+            this->_cargo.erase(vit);
+            success = true;
+            break;
+        }
+    }
 }
 
 void vessel_entity::serialize(QDataStream &output) {
--- sea_transport/entities/vessel_entity.h
+++ sea_transport/entities/vessel_entity.h
@@ -11,18 +11,22 @@ private:
     static entity_id __global_id;
 
     entity_id _id;
+    entity_id _skipper_id;
     entity_id _harbor_id;
     unsigned int _capacity;
     QVector<cargo_entity> _cargo;
 
 public:
     vessel_entity() = default;
-    vessel_entity(entity_id harbor_id, unsigned int capacity);
+    vessel_entity(entity_id skipper_id, entity_id harbor_id, unsigned int capacity);
 
     entity_id id() const;
     entity_id harbor() const;
     unsigned int capacity() const;
     const QVector<cargo_entity> cargo();
+
+    void add_cargo(cargo_entity object, bool &success);
+    void withdraw_cargo(entity_id oid, bool &success);
 
     void serialize(QDataStream &output);
     void deserialize(QDataStream &input);
