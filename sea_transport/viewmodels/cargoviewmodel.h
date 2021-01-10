9306786beb47674eb37c2849e9f823579c94e8db
Andrew nuark G <mrcluster@ya.ru>
2021-01-10 21:24:06 +0700

    Add docs for viewmodels

--- sea_transport/viewmodels/cargoviewmodel.h
+++ sea_transport/viewmodels/cargoviewmodel.h
@@ -7,19 +7,61 @@
 #include "entities/cargo_entity.h"
 
 
+/**
+ * @brief Cargo ViewModel class
+ * 
+ */
 class CargoViewModel : public QAbstractTableModel {
 private:
     Q_OBJECT
 
+    //! Collection of cargo for ViewModel
     QVector<cargo_entity> _data;
 
 public:
+    /**
+     * @brief Construct a new Cargo View Model object
+     * 
+     * @param parent [ignored]
+     */
     CargoViewModel(QObject *parent);
+    /**
+     * @brief Returns row count
+     * 
+     * @param parent [ignored]
+     * @return int 
+     */
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    /**
+     * @brief Returns column count
+     * 
+     * @param parent [ignored]
+     * @return int 
+     */
     int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    /**
+     * @brief Returns data about header
+     * 
+     * @param section header section
+     * @param orientation header orientation
+     * @param role header cell role
+     * @return QVariant 
+     */
     QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    /**
+     * @brief Returns data about cells
+     * 
+     * @param index cell index
+     * @param role draw role
+     * @return QVariant 
+     */
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 
+    /**
+     * @brief Sets cargo collection
+     * 
+     * @param new_data 
+     */
     void set_data(const QVector<cargo_entity> &new_data);
 };
 
--- sea_transport/viewmodels/vesselsviewmodel.h
+++ sea_transport/viewmodels/vesselsviewmodel.h
@@ -6,18 +6,58 @@
 #include <QAbstractTableModel>
 
 
+/**
+ * @brief Vessels ViewModel class
+ * 
+ */
 class VesselsViewModel : public QAbstractTableModel {
 private:
     Q_OBJECT
 
 public:
+    /**
+     * @brief Construct a new Vessels View Model object
+     * 
+     * @param parent [ignored]
+     */
     VesselsViewModel(QObject *parent = nullptr);
+    /**
+     * @brief Returns row count
+     * 
+     * @param parent [ignored]
+     * @return int 
+     */
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    /**
+     * @brief Returns column count
+     * 
+     * @param parent [ignored]
+     * @return int 
+     */
     int columnCount(const QModelIndex &parent = QModelIndex()) const;
+    /**
+     * @brief Returns data about header
+     * 
+     * @param section header section
+     * @param orientation header orientation
+     * @param role header cell role
+     * @return QVariant 
+     */
     QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    /**
+     * @brief Returns data about cells
+     * 
+     * @param index cell index
+     * @param role draw role
+     * @return QVariant 
+     */
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 
 public slots:
+    /**
+     * @brief Update slot, may be called then redraw needed
+     * 
+     */
     void update();
 };
 
